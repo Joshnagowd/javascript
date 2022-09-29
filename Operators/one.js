3c26be31ce16f94f3491c43103cb0f29504ef19b
Joshnagowd <joshigowdramagowni721@gmail.com>
2022-09-29 12:14:02 +0530

    Operators

---
+++ Operators/one.js
@@ -0,0 +1,27 @@
+// Arthematic
+let a=10
+let b=20
+let c="Joshna"
+let d;
+let e=40.5
+let g="Ram"
+let f="Krishna"
+let i="false"
+console.log(a + d);//NAN
+console.log(a + i);//NAN
+console.log(a + f);//NAN
+console.log(a + e);//NAN
+
+//constant
+const x = 10
+
+const y =20
+console.log( x + y)
+
+
+let j=10;
+let w="Rajini kanth";
+console.log(w)
+
+
+
---
+++ Operators/three.js
@@ -0,0 +1,42 @@
+let j=10;
+let g="Rajini kanth";
+console.log(j)
+// both multiplication and addition
+let a = 10
+let b = '20'
+let c = true
+console.log(a+b);//'1020'-concatination
+console.log(a*b);//200
+console.log(a+c)
+console.log(a*c)
+//relational operator
+let r = 100
+let s = 200
+let t = 1;
+console.log(r < s)
+console.log(t > t)
+console.log(t <= t)
+console.log(r >= t)
+
+
+//==check only content
+//==verify first data type and content
+let e = 10
+let f = '10'
+console.log(e == f)
+console.log(e === f)
+let h  = "Rahul"
+let i = "rahu"
+console.log(h == i)
+console.log(h === i)
+
+//seventh one
+let x ='10'
+let y ='10'
+console.log(x == y)
+console.log(x === y)
+
+
+
+
+
